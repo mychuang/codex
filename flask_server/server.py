49398eb3b9c4aa8068e3fb7dff4e4740eefc7ca9
mychuang <hhjoy222@gmail.com>
2025-06-14 21:43:47 +0800

    新增部屬 flask 至 Render 的參數檔

--- flask_server/server.py
+++ flask_server/server.py
@@ -45,8 +45,8 @@ def postInput():
         return jsonify({'error': str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # 必須讓 Render 控制 PORT
+    app.run(host="0.0.0.0", port=port)
 
     
